b43efed82876ccec7d3403f6fc29f6fa338214e3
MazenShebeta <mazenshebeta@gmail.com>
2023-11-17 01:26:32 +0200

    prettier

--- Controllers/QrCodeController.js
+++ Controllers/QrCodeController.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
-const qrcode = require('qrcode');
+const fs = require("fs");
+const qrcode = require("qrcode");
 
-class QrCode{
+class QrCode {
   static async generateQRCode(req, res) {
     try {
+      if (!req.body.text) throw new Error("Text is required!");
       const pngBuffer = await qrcode.toBuffer(req.body.text);
-      res.type('png');
+      res.type("png");
       res.send(pngBuffer);
     } catch (error) {
-      console.error('Error generating QR code:', error.message);
+      console.error("Error generating QR code:", error.message);
       res.status(500).json({ message: error.message });
     }
   }
-
 }
 
 module.exports = QrCode;
