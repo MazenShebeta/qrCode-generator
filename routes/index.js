0c2f48de7a861a98050fa04b653389425f14df2c
MazenShebeta <mazenshebeta@gmail.com>
2023-11-17 01:37:22 +0200

    .js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const QrCodeRouter  = require('./qrCodeRouter');
+const QrCodeRouter  = require('./qrCodeRouter.js');
 
 router.use('/qrcode', QrCodeRouter);
 
--- routes/qrCodeRouter.js
+++ routes/qrCodeRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const QrCodeController = require('../controllers/QrCodeController');
+const QrCodeController = require('../controllers/QrCodeController.js');
 
 router.post('/', QrCodeController.generateQRCode);
 
